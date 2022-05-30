5a34ae03bce956a5c097a1764509a4cbcc2e5aa5
aswanzakky <aswanzakky@mail.ugm.ac.id>
2022-05-30 18:15:00 +0700

    fix: verify email function

--- server/middlewares/auth/verifySignUp.js
+++ server/middlewares/auth/verifySignUp.js
@@ -3,32 +3,19 @@ const UserAccount = db.userAccount;
 
 // Function to Check Whether Duplicate Email is Exist
 checkDuplicateUsernameOrEmail = (req, res, next) => {
-  // Username
+  // Email
   UserAccount.findOne({
-    username: req.body.username,
+    email: req.body.email,
   }).exec((err, user) => {
     if (err) {
       res.status(500).send({ message: err });
       return;
     }
     if (user) {
-      res.status(400).send({ message: "Failed! Username is already in use!" });
+      res.status(400).send({ message: "Failed! Email is already in use!" });
       return;
     }
-    // Email
-    UserAccount.findOne({
-      email: req.body.email,
-    }).exec((err, user) => {
-      if (err) {
-        res.status(500).send({ message: err });
-        return;
-      }
-      if (user) {
-        res.status(400).send({ message: "Failed! Email is already in use!" });
-        return;
-      }
-      next();
-    });
+    next();
   });
 };
 
